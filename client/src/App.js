c2333626a63985f65eb1c191ddf5357257a4ebb7
hkr0416 <hkr0416@gmail.com>
2022-02-10 15:46:20 +0900

    server -> img tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-//import { useState } from 'react';
+// import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
@@ -7,20 +7,22 @@ function App() {
 
   const onSubmit = (e) => {    
     e.preventDefault();
-    console.dir(e.target[0])
+    console.dir(e)
     formData.append('image', e.target[0].files[0]);
     axios.post('http://localhost:8080',
       formData,
-      { 'content-type' : 'multipart/form-data' }      
+      { /* 'content-type' : 'multipart/form-data' */ }      
       )
   };
 
-
   return (
-    <form /* method='post' action='http://localhost:8080' encType='multipart/form-data' */ onSubmit={onSubmit }>
-      <input type="file" name='image' accept='image/*'/>
-      <button type='submit'>전송</button>
-    </form>
+    <div>
+      <img src='http://localhost:8080/img' />
+      <form /* method='post' action='http://localhost:8080' encType='multipart/form-data' */ onSubmit={onSubmit}>
+        <input type="file" name='image' accept='image/*'/>
+        <button type='submit'>전송</button>
+      </form>
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { triggerAsyncId } = require('async_hooks');
 const multer = require('multer');
+const fs = require('fs');
 
 const port = 8080;
 const app = express();
@@ -10,15 +11,20 @@ const upload = multer({
     dest : 'uploads/'
 })
 
-
 app.use(cors( { origin : 'http://localhost:3000' } ));
 app.use(express.json());
 
-app.post('/', upload.single('image'), async (req, res) => {
-    
+app.get('/img', (req, res) => {
+    fs.readFile('./uploads/sample2.jpeg', (error, data) => {
+        res.status(200).set({'Content-Type' : 'text/html'}).send(data);
+    })
+})
+
+app.post('/', upload.single('image'), (req, res) => {    
     try{
         console.log(req.headers);
         console.log(req.body);
+
         console.log(req.file);
 
         res.status(201).send('ok');
@@ -27,7 +33,6 @@ app.post('/', upload.single('image'), async (req, res) => {
         console.log(err);
         res.status(400).send('error');
     }
-
 })
 
 app.listen(port, () => {
